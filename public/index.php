8653bd36eebd13ed3b536709a9254c24d54645d4
Axelle <guediaaxelle@gmail.com>
2026-08-07 10:08:10 -0700

    Fix APP_ROOT for Azure

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 
 // On InfinityFree (and most shared hosts), FTP root IS htdocs.
 // Everything lives inside htdocs/, so APP_ROOT = this file's directory.
-define('APP_ROOT', __DIR__);
+define('APP_ROOT', dirname(__DIR__));
 
 // Autoloader
 spl_autoload_register(function (string $class): void {
